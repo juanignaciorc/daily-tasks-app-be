837150bba33a7d13c877d1a090be285692831b90
Juan Ignacio Roldan <juanignacioroldan01@gmail.com>
2021-06-10 17:46:05 -0300

    feature sign up implemented squashed commit

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from 'src/auth/auth.service';
+import { AuthCredentialsDto } from 'src/auth/dto/auth-credentials.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authServices: AuthService) {}
+
+  @Post('/signup')
+  signUp(@Body() authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    return this.authServices.signUp(authCredentialsDto);
+  }
+}
--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,7 +1,28 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { User } from 'src/auth/user.entity';
+import { AuthCredentialsDto } from 'src/auth/dto/auth-credentials.dto';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 
 @EntityRepository(User)
 export class UsersRepository extends Repository<User> {
-  // asdsad
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const user = this.create({ username, password: hashedPassword });
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
 }
